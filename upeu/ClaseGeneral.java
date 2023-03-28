d1770e43c8aa1463219aa2d6851b7af57e658096
dmamanipar <dmamanipar@unsa.edu.pe>
2023-03-28 22:16:06 +0000

    aplicando encampsulamiento

--- upeu/ClaseGeneral.java
+++ upeu/ClaseGeneral.java
@@ -4,7 +4,13 @@ public class ClaseGeneral {
     public static void main(String[] args) {
         Persona person=new Persona();       
         person.edad=19;
+        person.nombre="Jose";
         person.saludo();
+
+        Persona person1=new Persona();
+        person1.setNombre("Raul");//Encampsulamiento
+        person1.setEdad(20);//Encampsulamiento
+        person1.saludo();     
         
     }
 }
--- upeu/Persona.java
+++ upeu/Persona.java
@@ -7,7 +7,17 @@ public class Persona {
 
     public String nombre="David";
     public int edad;
-
+    public String dni;
+    //previo para Encampsulamiento
+    public String getDni(){return dni;}
+    public void setDni(String dni){this.dni=dni;}
+    
+    public String getNombre() {return nombre;}
+    public void setNombre(String nombre) {this.nombre = nombre;}
+    
+    public int getEdad() {return edad;}
+    public void setEdad(int edad) {this.edad = edad;}    
+    //Fin para Encampsulamiento
     public void saludo() {
         System.out.println("Hola, soy "+nombre+" y mi edad es:"+edad);
     }
