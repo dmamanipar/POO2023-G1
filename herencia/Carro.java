ed33e3a3c45a3a6c2ff88e925b84321a2efeae9e
dmamanipar <dmamanipar@unsa.edu.pe>
2023-03-28 23:02:58 +0000

    Uso de memoria en Java

--- herencia/Carro.java
+++ herencia/Carro.java
@@ -2,6 +2,11 @@ package herencia;
 
 public class Carro extends Vehiculo{//Clase hijo
     public  String modelo="Mustang";
+    public int anho_fab=2020;
+    public long vigencia_soat=1;
+
+    public char vigencoa_soat_r=1;
+    public short vigencia_soat_m=11;
 
     public static void main(String[] args) {
         Carro objCar=new Carro();
---
+++ usomemoria/UsoMemoria.java
@@ -0,0 +1,15 @@
+package usomemoria;
+
+public class UsoMemoria {
+   static Persona p1;
+   static int cantidad=30;
+    public static void main(String[] args) {
+        p1=new Persona();
+        p1.nombre="Dario";
+        p1.dni="34631917";
+    }
+}
+
+class Persona {
+    public String nombre, dni; 
+}
