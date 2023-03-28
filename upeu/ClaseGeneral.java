4e3262c46fac841dbc55d3f14afee966bf231b8b
dmamanipar <dmamanipar@unsa.edu.pe>
2023-03-28 21:50:08 +0000

    ejemplo de clase y objeto 2

---
+++ upeu/ClaseGeneral.java
@@ -0,0 +1,10 @@
+package upeu;
+
+public class ClaseGeneral {
+    public static void main(String[] args) {
+        Persona person=new Persona();       
+        person.edad=19;
+        person.saludo();
+        
+    }
+}
---
+++ upeu/Persona.java
@@ -0,0 +1,15 @@
+package upeu;
+
+/**
+ * Persona
+ */
+public class Persona {
+
+    public String nombre="David";
+    public int edad;
+
+    public void saludo() {
+        System.out.println("Hola, soy "+nombre+" y mi edad es:"+edad);
+    }
+    
+}
