0292711c651ccfb0e532255f67dbf56d97df0f09
dmamanipar <dmamanipar@unsa.edu.pe>
2023-05-09 17:42:23 -0500

    CRUD completo en Consola con SQLite

--- SysAdmisionG1/src/main/java/pe/edu/upeu/app/dao/PostulanteDao.java
+++ SysAdmisionG1/src/main/java/pe/edu/upeu/app/dao/PostulanteDao.java
@@ -136,47 +136,78 @@ public class PostulanteDao implements PostulanteDaoI {
             throw new Exception("Detalle:" + ex.getMessage());
         }
         return comit;
-    }    
-        
+    }
+
     public static void main(String[] args) {
-        Scanner cs=new Scanner(System.in);
+        Scanner cs = new Scanner(System.in);
         PostulanteDao po = new PostulanteDao();
-        
+
         int i = 0;
-        String opcion="R";
-        String mensajeOpciones="Menu de Opciones\nR=Reportar\nC=Crear\nU=Update\nD=Delete\nX=Salir";        
-        do{
+        String opcion = "R";
+        String mensajeOpciones = "Menu de Opciones\nR=Reportar\nC=Crear\nU=Update\nD=Delete\nX=Salir";
+        do {
             switch (opcion) {
                 case "C" -> {
+                    PostulanteTO tox = new PostulanteTO();
+                    System.out.println("Ingrese el DNI:");
+                    tox.setDni(cs.next());
+                    System.out.println("Ingres Nombre:");
+                    tox.setNombre(cs.next());
+                    System.out.println("Ingres A. Paterno:");
+                    tox.setApellidoPat(cs.next());
+                    System.out.println("Ingres A. Materno:");
+                    tox.setApellidoMat(cs.next());
+                    System.out.println("Ingrese Modalidad(E=Examen, PI=Primeros Puestos):");
+                    tox.setModalidad(cs.next());
+                    System.out.println("Ingrese Estado (Activo, Desactivo):");
+                    tox.setEstado(cs.next());
+                    System.out.println("Ingrese el Perido(1=2023-1):");
+                    tox.setIdPeriodo(cs.nextInt());
+                    System.out.println("Ingrese Carrera(1=Sistemas, 2=Contabilidad):");
+                    tox.setIdCarrera(cs.nextInt());
+                    po.create(tox);
+                    po.listarPostulantes(po.listarTodo());
                 }
-                case "R" -> po.listarPostulantes(po.listarTodo());
+                case "R" ->
+                    po.listarPostulantes(po.listarTodo());
                 case "U" -> {
+                    PostulanteTO tox;
+                    System.out.println("Ingrese el DNI a Modificar:");
+                    String dni=cs.next();
+                    tox=po.buscarEntidad(dni);
+                    System.out.println("Ingres Nuevo Nombre:");
+                    tox.setNombre(cs.next());
+                    System.out.println("Ingres Nuevo A. Paterno:");
+                    tox.setApellidoPat(cs.next());                    
+                    po.update(tox);
+                    po.listarPostulantes(po.listarTodo());
                 }
                 case "D" -> {
                     try {
                         System.out.println("Ingrese el DNI del Registro que desea eliminar:");
                         po.delete(cs.next());
                         po.listarPostulantes(po.listarTodo());
-                    } catch (Exception e) { System.err.println("Error al Eliminar"); }
-                }                    
-                default -> System.out.println("Opcion no existe intente otra vez!");
+                    } catch (Exception e) {
+                        System.err.println("Error al Eliminar");
+                    }
+                }
+                default ->
+                    System.out.println("Opcion no existe intente otra vez!");
             }
-            System.out.println("Que desea hacer:\n"+mensajeOpciones);
-            opcion=cs.next();
-        }while(!opcion.toUpperCase().equals("X"));
-        
+            System.out.println("Que desea hacer:\n" + mensajeOpciones);
+            opcion = cs.next();
+        } while (!opcion.toUpperCase().equals("X"));
+
         System.out.println("F1:" + (i++));
     }
-    
-    public void listarPostulantes(List<PostulanteTO> lista){
+
+    public void listarPostulantes(List<PostulanteTO> lista) {
         System.out.println("DNI\t\tNombre\t\t\tApellidos\t\t\tCarrera Post.");
-        for (PostulanteTO p: lista) {
-            System.out.println(p.getDni()+"\t"+p.getNombre()+"\t\t\t"
-                    +p.getApellidoPat()+" "+p.getApellidoMat()+"\t\t\t"+p.getNombreCarrera());
+        for (PostulanteTO p : lista) {
+            System.out.println(p.getDni() + "\t" + p.getNombre() + "\t\t\t"
+                    + p.getApellidoPat() + " " + p.getApellidoMat() + "\t\t\t" + p.getNombreCarrera());
         }
     }
-
-
 
     @Override
     public List<PostulanteTO> listCmb(String filter) {
@@ -185,7 +216,32 @@ public class PostulanteDao implements PostulanteDaoI {
 
     @Override
     public PostulanteTO buscarEntidad(String dni) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        PostulanteTO cli = new PostulanteTO();
+        String sql = "SELECT po.*, p.nombre as nombreperiodo, c.nombrecarrera "
+                + "FROM postulante po, periodo p, carrera c "
+                + "WHERE p.id_periodo = po.id_periodo and po.id_carrera = c.id_carrera"
+                + " and po.dni=?";
+        try {
+            ps = connection.prepareStatement(sql);
+            ps.setString(1, dni);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                cli.setDni(rs.getString("dni"));
+                cli.setNombre(rs.getString("nombre"));
+                cli.setApellidoPat(rs.getString("apellido_pat"));
+                cli.setApellidoMat(rs.getString("apellido_mat"));
+                cli.setModalidad(rs.getString("modalidad"));
+                cli.setEstado(rs.getString("estado"));
+                cli.setIdCarrera(rs.getInt("id_carrera"));
+                cli.setIdPeriodo(rs.getInt("id_periodo"));
+                cli.setNombrePeriodo(rs.getString("nombreperiodo"));
+                cli.setNombreCarrera(rs.getString("nombrecarrera"));
+                //cli.setNombreModalidad(buscarModalidadExamen(rs.getString("modalidad")));               
+            }
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        }
+        return cli;
     }
 
     @Override
