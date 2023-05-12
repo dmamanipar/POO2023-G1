a528bda1b5104852a872d57637e6db0d1b9a50b2
Patricia-Apaza <patriciaapaza80@gmail.com>
2023-05-12 05:48:27 -0500

    SysAdmisionG1

---
+++ SysAdmisionG1/src/main/java/pe/edu/upeu/app/dao/Resultado_finalDaoI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package pe.edu.upeu.app.dao;
+
+import java.util.List;
+import pe.edu.upeu.app.modelo.Resultado_finalTO;
+
+/**
+ *
+ * @author HP
+ */
+public interface Resultado_finalDaoI {
+    public List<Resultado_finalTO> ListarTodo();
+}
---
+++ SysAdmisionG1/src/main/java/pe/edu/upeu/app/modelo/Resultado_finalTO.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.upeu.app.modelo;
+
+import lombok.Data;
+
+
+/**
+ *
+ * @author HP
+ */
+
+@Data
+public class Resultado_finalTO {
+    public int idresultfinal, idpostulante, idperiodo, idcarrera, idevalpsicologica;
+    public String dni, punto_conocimiento, punto_entrevista;
+}
