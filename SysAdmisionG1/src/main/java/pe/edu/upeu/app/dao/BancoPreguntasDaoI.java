77e85365b959c2d9b30e485f093f0ccc5d8a3c48
jose2207 <armandomamanip@gmail.com>
2023-05-16 20:20:49 -0500

    D:\GOYIN\POO2023-G1\SysAdmisionG1

---
+++ SysAdmisionG1/src/main/java/pe/edu/upeu/app/dao/BancoPreguntasDaoI.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package pe.edu.upeu.app.dao;
+
+import java.util.List;
+import pe.com.syscenterlife.autocomp.ModeloDataAutocomplet;
+import pe.edu.upeu.app.modelo.BancoPreguntasTO;
+import pe.edu.upeu.app.modelo.ComboBoxOption;
+import pe.edu.upeu.app.modelo.PostulanteTO;
+/**
+ *
+ * @author Jose MP
+ */
+public interface BancoPreguntasDaoI {
+    public int create(BancoPreguntasTO d);
+public int update(BancoPreguntasTO d);
+public int delete(String id) throws Exception;
+public List<BancoPreguntasTO> listCmb(String filter);
+public List<BancoPreguntasTO> listarTodo();
+public BancoPreguntasTO buscarEntidad(String id_area);
+public List<ModeloDataAutocomplet> listAutoComplet(String filter); 
+public List<ComboBoxOption> listaModalidadExamen();
+public List<ComboBoxOption> listarPeriodo();
+public List<ModeloDataAutocomplet> listAutoCompletCarrera(String filter);
+public String buscarModalidadExamen(String id);
+}
---
+++ SysAdmisionG1/src/main/java/pe/edu/upeu/app/modelo/BancoPreguntasTO.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.edu.upeu.app.modelo;
+
+import lombok.Data;
+
+/**
+ *
+ * @author Data_Science
+ */
+@Data
+public class BancoPreguntasTO {
+    public int idBancoPreguntas,idArea;
+    public String pregunta;
+    
+}
