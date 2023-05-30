3720d70725a6fb85eae484815089ba5392bd2ad4
Lab-Software <you@example.com>
2023-05-30 17:43:29 -0500

    GUI main preguntas

--- SysAdmisionG1/src/main/java/pe/edu/upeu/app/gui/GUIMain.java
+++ SysAdmisionG1/src/main/java/pe/edu/upeu/app/gui/GUIMain.java
@@ -123,6 +123,19 @@ public class GUIMain extends JFrame {
         public void actionPerformed(ActionEvent e) {
             System.out.println("pasa por aqui");
             Container contai = GUIMain.this.getContentPane();
+            if(((JMenuItem)e.getSource()).getName().equals("mipreguntas")){
+                //System.out.println("hola si llega");
+                jtpane.removeAll();
+                MainPreguntas map=new MainPreguntas();
+                map.setPreferredSize(new Dimension(1000,800));
+                scrollPane=new JScrollPane(map);
+                scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+                scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+                jtpane.add(scrollPane, "Preguntas");
+                contai.add(BorderLayout.CENTER, jtpane);
+                contai.validate();
+                contai.repaint();
+            }
             if (((JMenuItem) e.getSource()).getName().equals("mimiselectall")) {
                 jtpane.removeAll();//remueve todo el contenido
                 /*MainPostulante jp = new MainPostulante();
